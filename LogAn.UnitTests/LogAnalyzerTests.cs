dd264af9fc2d69a2293d5548d1cee4973e532bcb
Danny Baker <dannym.baker@gmail.com>
2022-12-14 10:02:07 -0500

    Initial release. Three tests & one production code method.

--- LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -10,12 +10,33 @@ namespace LogAn.UnitTests
     [TestFixture]
     public class LogAnalyzerTests
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
         [Test]
-        public void IsValidFileName_BadExtension_ReturnsFalse()
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
             bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
-            Assert.IsFalse(result);
+            Assert.IsFalse(result);            
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+            Assert.IsTrue(result);
         }
     }
 }
